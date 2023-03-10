2e447d7416396de7f6fd0c67a5e24b0b98b80720
Thayná <tfal@discente.ifpe.edu.br>
2023-03-10 10:02:34 -0300

    versão inicial do projeto

--- loja/admin.py
+++ loja/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Fornecedor, Categoria, Cliente
+from .models import Fornecedor, Categoria, Cliente, Loja
 
 admin.site.register(Fornecedor)
 admin.site.register(Categoria)
 admin.site.register(Cliente)
+admin.site.register(Loja)
--- loja/models.py
+++ loja/models.py
@@ -20,3 +20,13 @@ class Cliente(models.Model):
     email = models.EmailField(max_length=100)
     CPF = models.CharField(max_length=30)
     CEP = models.CharField(max_length=11)
+
+class Loja(models.Model):
+    nome=models.CharField(max_length=120)
+    endereco= models.CharField(max_length=120)
+    cnpj= models.CharField(max_length=120)
+    cidade= models.CharField(max_length=120)
+    estado= models.CharField(max_length=2)
+    cep= models.CharField(max_length=11)
+    email= models.EmailField(max_length=100)
+    telefone= models.CharField(max_length=120)
