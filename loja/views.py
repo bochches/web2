bb152b1091b097080e2faa546eab4a84f447e9dc
Thayná <tfal@discente.ifpe.edu.br>
2023-03-28 10:05:04 -0300

    versão inicial do projeto

--- loja/views.py
+++ loja/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from  rest_framework.renderers import  TemplateHTMLRenderer
 from  rest_framework.views import  APIView
+
+class IndexPage(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'page/index.html'
+    #resgatar o html
+
+    def get(self, request):
+        return Response({})
